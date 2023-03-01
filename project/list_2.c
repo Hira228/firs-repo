ea0331fc15105d74b12a833c909de8f759d84377
Hira228 <cafenero19@gmail.com>
2023-03-01 22:02:57 +0300

    print back, print list, print debug

---
+++ project/list_2.c
@@ -0,0 +1,58 @@
+//Пытаюсь разобраться со структурами и двусвязными списками :))
+// Двусвязный циклический список с барьерным элементом
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list_2.h"
+
+void print_list(struct Node *list);
+void print_back(struct Node *list);
+void print_dbg(struct Node *list);
+
+int main()
+{
+    struct Node z, a = {3}, b = {17}, c = {21};
+    struct Node *list = &z;
+    z.next = &a;
+    z.prev = &c;
+    a.next = &b;
+    a.prev = &z;
+    b.next = &c;
+    b.prev = &a;
+    c.next = &z;
+    c.prev = &b;
+
+    print_list(list);
+    print_back(list);
+    print_dbg(list);
+}
+
+void print_list(struct Node *list)
+{
+    for(struct Node *ptr = list -> next; ptr != list; ptr = ptr -> next)
+    {
+        printf("%d ", ptr -> data);
+    }
+    printf("\n");
+}
+
+void print_back(struct Node *list)
+{
+    for(struct Node *ptr = list -> prev; ptr != list; ptr = ptr -> prev)
+    {
+        printf("%d ", ptr -> data);
+    }
+    printf("\n");
+}
+
+void print_dbg(struct Node *list)
+{
+    struct Node *ptr = list;
+    printf("--------------------------------------------------------------------\n");
+    printf("list:  prev=%p %p next=%p\n", ptr -> prev, ptr, ptr -> next);
+    for(ptr = list -> next; ptr != list; ptr = ptr -> next)
+    {
+        printf("%d prev=%p %p next=%p\n", ptr -> data, ptr -> prev, ptr, ptr -> next);
+    }
+    printf("\n");
+}
