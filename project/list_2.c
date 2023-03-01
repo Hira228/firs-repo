2d25be0bc1d930d691317ef653ea03494f70bd4d
Hira228 <cafenero19@gmail.com>
2023-03-01 22:12:34 +0300

    insert model

--- project/list_2.c
+++ project/list_2.c
@@ -8,11 +8,17 @@
 void print_list(struct Node *list);
 void print_back(struct Node *list);
 void print_dbg(struct Node *list);
+void insert_node(struct Node *list);
 
 int main()
 {
-    struct Node z, a = {3}, b = {17}, c = {21};
+    struct Node z, a = {3}, b = {17}, c = {21}, u = {10};
     struct Node *list = &z;
+    z.data = 0;
+    a.data = 3;
+    b.data = 17;
+    c.data = 21;
+    u.data = 10;
     z.next = &a;
     z.prev = &c;
     a.next = &b;
@@ -22,9 +28,17 @@ int main()
     c.next = &z;
     c.prev = &b;
 
-    print_list(list);
-    print_back(list);
+    print_list(list);  // 3 17 21
+    print_back(list);  // 21 17 3
     print_dbg(list);
+    
+    u.prev = &a;  // 1
+    u.next = &b;  // 2
+    a.next = &u;  // 3
+    b.prev = &u;  // 4
+
+    print_list(list);  // 3 10 17 21
+    print_back(list);  // 21 17 10 3
 }
 
 void print_list(struct Node *list)
