ed62aefcbc45860025375b79306a71391c073a87
Hira228 <cafenero19@gmail.com>
2023-03-02 16:54:51 +0300

    insert node before

--- project/list_2.c
+++ project/list_2.c
@@ -9,16 +9,18 @@ void print_list(struct Node *list);
 void print_back(struct Node *list);
 void print_dbg(struct Node *list);
 void insert_node(struct Node *p, struct Node *t);
+void insert_node_before(struct Node *p, struct Node *t);
 
 int main()
 {
-    struct Node z, a = {3}, b = {17}, c = {21}, u = {10};
+    struct Node z, a = {3}, b = {17}, c = {21}, u = {10} , w = {8};
     struct Node *list = &z;
     z.data = 0;
     a.data = 3;
     b.data = 17;
     c.data = 21;
     u.data = 10;
+    w.data = 8;
     z.next = &a;
     z.prev = &c;
     a.next = &b;
@@ -39,6 +41,14 @@ int main()
 
     print_list(list);  // 3 10 17 21
     print_back(list);  // 21 17 10 3
+
+    p = &u;
+    t = &w;
+
+    insert_node_before(&u, &w);
+
+    print_list(list);   // 3 8 10 17 21
+    print_back(list);   // 21 17 10 8 3
 }
 
 void print_list(struct Node *list)
@@ -79,4 +89,14 @@ void insert_node(struct Node *p, struct Node *t)
     t -> next = q;  // 2
     p -> next = t;  // 3
     q -> prev = t;  // 4
+}
+
+void insert_node_before(struct Node *p, struct Node *t)
+{
+    struct Node *q = p -> prev;
+
+    t -> next = p;  // 1
+    t -> prev = q;  // 2
+    p -> prev = t;  // 3
+    q -> next = t;  // 4
 }
