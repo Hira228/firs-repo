c86dc842a6d0a36bf7f723dd5ce5a6f33b441080
Hira228 <cafenero19@gmail.com>
2023-03-11 00:07:06 +0300

    finish list and tests

--- project/T11D17-0-develop/src/list.h
+++ project/T11D17-0-develop/src/list.h
@@ -4,6 +4,8 @@ struct node *init (struct door *door);
 struct node *add_door (struct node *elem, struct door *door);
 struct node *find_door(int door_id, struct node *root);
 void destroy(struct node* root);
+void print_list(struct node *elem);
+int list_is_empty(struct node *elem);
 
 struct node{
     struct node *next;
