7802eac8e8f8edec236965210794b61e28442510
Hira228 <cafenero19@gmail.com>
2023-03-13 20:28:12 +0300

    list_insert

--- project/problems/list_stack_struct/2/list.c
+++ project/problems/list_stack_struct/2/list.c
@@ -5,7 +5,8 @@
 int main()
 {
     struct door first_elem = {1, 0};
-    struct door tests[] = {{2, 0}, {3, 0}, {4, 0}};
+    struct door tests[] = {{1, 0}, {2, 0}, {3, 0}, {4, 0}};
+    struct node *new_insert_elem = NULL;
     struct door *door = &first_elem;
     struct node *elem = NULL;
     struct node *find_elem = NULL;
@@ -13,14 +14,13 @@ int main()
 
     printf("Состояние списка :%s\n", list_is_empty(elem) ? "Список пуст\n" : "Список не пуст\n");
 
-    elem = init(door);
     printf("Состояние списка :%s\n", list_is_empty(elem) ? "Список пуст\n" : "Список не пуст\n");
     print_list(elem);
 
     for (int i = 0; i < sizeof(tests) / sizeof(tests[0]); i++)
     {
         door = &tests[i];
-        elem = add_door(elem, door);
+        add_door(&elem, door);
         printf ("Добавлен новый элемент списка: %d %d\n", elem -> door.id, elem -> door.status);  // Вывод добавленного элемента door в список  1 2 | 3 4 | 5 6
     }
     
@@ -37,7 +37,11 @@ int main()
 
     printf("\n");
 
-    destroy(elem);
+    destroy(&elem);
+
+ 
+    new_insert_elem = insert(elem, 0, 111);
+    print_list(elem);
 }
 
 int list_is_empty(struct node *elem)
@@ -45,13 +49,12 @@ int list_is_empty(struct node *elem)
     return elem == NULL;
 }
 
-struct node *init (struct door *door)
+void init (struct door *door)
 {
     struct node *ptr = malloc(sizeof(struct node));
     ptr -> door.id = door -> id;
     ptr -> door.status = door -> status;
     ptr -> next = NULL;
-    return ptr;
 }
 
 void print_list(struct node *elem)
@@ -63,33 +66,56 @@ void print_list(struct node *elem)
     }
 } 
 
-struct node *add_door (struct node *elem, struct door *door)
+void add_door (struct node **elem, struct door *door)
 {
     struct node *ptr = malloc(sizeof(struct node));
-    ptr -> next = elem;
+    ptr -> next = *elem;
     ptr -> door.id = door -> id;
     ptr -> door.status = door -> status;
-    elem = ptr;
-    return elem;
+    *elem = ptr;
 }
 
-struct node *find_door(int door_id, struct node *root)
+struct node  *find_door(int door_id, struct node *root)
 {
     while (root -> door.id != door_id)
     {
         root = root -> next;
     }
-
     return root;
 }
 
-void destroy(struct node* root)
+void destroy(struct node** root)
 {
-    struct node *ptr = root;
-    root = ptr -> next;
+    struct node *ptr = *root;
+    *root = ptr -> next;
     printf("Удален элемент списка:\n\n|%d %d|\n\n", ptr -> door.id, ptr -> door.status);
     printf("Вывод всего списка: \n");
-    print_list(root);
-    printf("Состояние списка :%s\n", list_is_empty(root) ? "Список пуст\n" : "Список не пуст\n");
+    print_list(*root);
+    printf("Состояние списка :%s\n", list_is_empty(*root) ? "Список пуст\n" : "Список не пуст\n");
     free(ptr);
+}
+
+struct node *insert(struct node *elem, int n, int value)
+{
+    int i = 0;
+    while (i < n && elem -> next != NULL)
+    {
+        elem = elem -> next;
+        i++;
+    }
+
+    struct node *ptr = malloc(sizeof(struct node));
+    ptr -> door.id = value;
+    ptr -> door.status = 0;
+
+    if(elem -> next != NULL)
+    {
+        ptr -> next = elem -> next;
+    }
+    else
+    {
+        ptr -> next = NULL;
+    }
+    elem -> next = ptr;
+    return ptr;
 }
--- project/problems/list_stack_struct/2/list.h
+++ project/problems/list_stack_struct/2/list.h
@@ -1,13 +1,14 @@
 #include "door_struct.h"
 
-struct node *init (struct door *door);
-struct node *add_door (struct node *elem, struct door *door);
-struct node *find_door(int door_id, struct node *root);
-void destroy(struct node* root);
-void print_list(struct node *elem);
-int list_is_empty(struct node *elem);
-
 struct node{
     struct node *next;
     struct door door;
 };
+
+void init (struct door *door);
+void add_door(struct node **elem, struct door *door);
+struct node *find_door(int door_id, struct node *root);
+void destroy(struct node** root);
+void print_list(struct node *elem);
+int list_is_empty(struct node *elem);
+struct node *insert(struct node *elem, int n, int value);
